18c5543bccbe61e737fa911d8f297e9814bfda03
Manish Warang <manish6385@gmail.com>
2022-10-09 23:41:54 +0530

    added security group for Load Balancer and webservers

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -15,3 +15,34 @@ module "vpc" {
   enable_nat_gateway           = var.enable_nat_gateway
   single_nat_gateway           = var.single_nat_gateway
 }
+
+module "lb_sg" {
+  source = "terraform-in-action/sg/aws"
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = [{
+    port        = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+}
+
+module "webserver_sg" {
+  source = "terraform-in-action/sg/aws"
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = [{
+    port            = 8080
+    security_groups = [module.lb_sg.security_group.id]
+    },
+    {
+      port        = 22
+      cidr_blocks = ["10.0.0.0/16"]
+  }]
+}
+
+module "database_sg" {
+  source = "terraform-in-action/sg/aws"
+  vpc_id = module.vpc.vpc_id
+  ingress_rules = [{
+    port            = 3306
+    security_groups = [module.webserver_sg.security_group.id]
+  }]
+}
